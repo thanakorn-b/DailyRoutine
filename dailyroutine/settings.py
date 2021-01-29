8541aa7535ec984161fb2c46319bbdcaeb12d68f
thangkub <thananakorn_boon@hotmail.com>
2021-01-29 16:48:11 +0700

    change dir

--- dailyroutine/settings.py
+++ dailyroutine/settings.py
@@ -66,8 +66,8 @@ ROOT_URLCONF = 'dailyroutine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [os.path.join(BASE_DIR, 'build')],
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
+        # 'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
 
 # REST Framework
